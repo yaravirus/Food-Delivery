41cc6df8490dc1a91304c83dca80442e8eb0e1dd
durgesh <streamvolt000gmail.com>
2025-08-26 00:59:36 +0530

    yes

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-$link = mysqli_connect("localhost", "root", "");
+$link = mysqli_connect("bdoozd8hy9piw85hedya-mysql.services.clever-cloud.com","ueyrd2wkbbkrc1ma", "pg1LlQIIyLOs97MknM5l","",3306);
 if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysqli_select_db($link, "food_ordering_system") or die("Database not found");
+mysqli_select_db($link, "bdoozd8hy9piw85hedya") or die("Database not found");
 
 ?>
